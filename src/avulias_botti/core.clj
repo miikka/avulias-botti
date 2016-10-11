835b28d9be7be4378ff033c20b3c25bebb538de3
Miikka Koskinen <miikka.koskinen@iki.fi>
2016-10-11 23:28:20 +0300

    New command: /anssikela

--- src/avulias_botti/core.clj
+++ src/avulias_botti/core.clj
@@ -16,16 +16,28 @@
   (api/send-text +token+ chat-id "Satakuntatalon huoltopäivystys puh. 050-2861"))
 
 (defn apua [chat-id]
-  (api/send-text +token+ chat-id "Osaan seuraavat asiat: /huoltomies"))
+  (api/send-text +token+ chat-id
+                 "Osaan seuraavat asiat: /anssikela, /huoltomies, /logorauma"))
 
 (defn logo-rauma [chat-id]
   (send-sticker +token+ chat-id (io/file "resources/rauma.webp")))
+
+(defn reply-to [message body]
+  (let [chat-id (-> message :chat :id)
+        options {:parse_mode "Markdown"}]
+    (api/send-text +token+ chat-id options body)))
+
+(defn anssi-kela [message]
+  (prn :msg message)
+  (let [name (-> message :from :first_name)]
+    (reply-to message (format "%s, Anssi Kela -nimesi on _%s Kela_." name name))))
 
 (handlers/defhandler bot-api
   (handlers/command "huoltomies" {{chat-id :id} :chat} (huoltomies chat-id))
   (handlers/command "apua" {{chat-id :id} :chat} (apua chat-id))
   (handlers/command "help" {{chat-id :id} :chat} (apua chat-id))
   (handlers/command "logorauma" {{chat-id :id} :chat} (logo-rauma chat-id))
+  (handlers/command "anssikela" message (anssi-kela message))
   (handlers/message message (println "Got message" message)))
 
 (defn start []
