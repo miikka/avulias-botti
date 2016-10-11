5af437f8be22dbaf446f33afd859a0213e15478d
Oleg Grenrus <oleg.grenrus@iki.fi>
2016-10-11 21:09:20 +0300

    Add logorauma

--- src/avulias_botti/core.clj
+++ src/avulias_botti/core.clj
@@ -13,9 +13,22 @@
 (defn apua [chat-id]
   (api/send-text +token+ chat-id "Osaan seuraavat asiat: /huoltomies"))
 
+(def logorauma-text (str
+  "```text\n"
+  "|\\_-^-_/|\n"
+  "| r | r |\n"
+  "|-------|\n"
+  "| r | r |\n"
+  " `~~v~~´\n"
+  "```"))
+
+(defn logorauma [chat-id]
+  (api/send-text +token+ chat-id {:parse_mode "Markdown"} logorauma-text))
+
 (handlers/defhandler bot-api
   (handlers/command "huoltomies" {{chat-id :id} :chat} (huoltomies chat-id))
   (handlers/command "apua" {{chat-id :id} :chat} (apua chat-id))
+  (handlers/command "logorauma" {{chat-id :id} :chat} (logorauma chat-id))
   (handlers/command "help" {{chat-id :id} :chat} (apua chat-id))
   (handlers/message message (println "Got message" message)))
 
